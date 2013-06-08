7af3f9424b9cb2a795de7c27c893cc4001cbe04e
Jungho Bang <xeron@bangtoven.com>
2013-06-08 11:42:46 +0900

    Initial Commit

---
+++ ScreenshotObserver/ScreenshotObserver.h
@@ -0,0 +1,13 @@
+//
+//  ScreenshotObserver.h
+//  ScreenshotObserver
+//
+//  Created by Xeron on 13. 6. 8..
+//  Copyright (c) 2013년 Bangtoven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScreenshotObserver : NSObject
+
+@end
---
+++ ScreenshotObserver/ScreenshotObserver.m
@@ -0,0 +1,13 @@
+//
+//  ScreenshotObserver.m
+//  ScreenshotObserver
+//
+//  Created by Xeron on 13. 6. 8..
+//  Copyright (c) 2013년 Bangtoven. All rights reserved.
+//
+
+#import "ScreenshotObserver.h"
+
+@implementation ScreenshotObserver
+
+@end
